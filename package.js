75130559835a02fd5e8fbccce24c043279d7713d
johdirr <johannesdirr@me.com>
2015-05-12 10:37:03 +0200

    updated version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'johdirr:meteor-git-rev',
-  version: '0.0.2',
+  version: '0.0.3',
   summary: 'Get the current git commit hash, tag or branch in Meteor',
   git: 'https://github.com/johdirr/meteor-git-rev.git',
   documentation: 'README.md'
@@ -15,7 +15,7 @@ Package.onUse(function(api) {
   api.use(['templating', 'minimongo'], 'client');
   api.use('mongo', 'server');
 
-  api.addFiles('lib/revision.js', ['server', 'client'])
+  api.addFiles('lib/collection.js', ['server', 'client'])
   api.addFiles('client/uiHelper.js', 'client');
   api.addFiles('server/git-rev.js', 'server');
 });
